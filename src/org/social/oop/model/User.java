d374839e09368d7649824819d556c7d27a45448e
Adryan Reis <88512599+Nasc1mento@users.noreply.github.com>
2022-12-22 14:15:03 -0300

    Update User.java

--- src/org/social/oop/model/User.java
+++ src/org/social/oop/model/User.java
@@ -4,7 +4,7 @@ import java.util.Objects;
 
 public class User {
 	
-	private Integer id;
+	private int id;
 	private String name;
 	private String email;
 	private String phone;
@@ -37,14 +37,14 @@ public class User {
 
 
 
-	public Integer getId() {
+	public int getId() {
 		return id;
 	}
 
 
 
 
-	public void setId(Integer id) {
+	public void setId(int id) {
 		this.id = id;
 	}
 
