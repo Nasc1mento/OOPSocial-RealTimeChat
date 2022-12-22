8a60630736deea08c6cf418225175a5427c25d95
Adryan Reis <88512599+Nasc1mento@users.noreply.github.com>
2022-12-22 14:17:14 -0300

    Create UserSession.java

---
+++ src/org/social/oop/UserSession.java
@@ -0,0 +1,35 @@
+package org.social.oop.sessao;
+
+import org.social.oop.model.User;
+
+public class UserSession {
+	
+	
+	
+	public static String nome;
+	public static String email;
+	public static boolean isLogged;
+	
+	
+	public UserSession() {
+		
+	}
+	
+	public static void logout() {
+		nome = null;
+		email = null;
+		isLogged = false;	
+	}
+	
+	public static void login(User user) {
+		
+		nome = user.getName();
+		email = user.getEmail();
+		isLogged = true;
+	}
+	
+	
+	
+	
+	
+}
